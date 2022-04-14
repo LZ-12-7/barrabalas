f09364509621482292f7cc7caece1b146f8dbf38
LZ <94126308+LZ-12-7@users.noreply.github.com>
2022-04-14 16:14:41 +0200

    Add files via upload

---
+++ barrabalas/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+game "gta5"
+author "LZ"
+
+description 'Clip'
+
+version '1.0.0'
+
+--[[server_scripts {
+  'server/sv_clip.lua'
+}]]--
+
+client_scripts {
+  'client/cl_clip.lua'
+}
