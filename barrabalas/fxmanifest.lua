7cb03b39090a1d321fe152f540545346f70b770d
LZ <94126308+LZ-12-7@users.noreply.github.com>
2022-04-14 16:14:58 +0200

    Update fxmanifest.lua

--- barrabalas/fxmanifest.lua
+++ barrabalas/fxmanifest.lua
@@ -2,14 +2,6 @@ fx_version "adamant"
 game "gta5"
 author "LZ"
 
-description 'Clip'
-
-version '1.0.0'
-
---[[server_scripts {
-  'server/sv_clip.lua'
-}]]--
-
 client_scripts {
   'client/cl_clip.lua'
 }
